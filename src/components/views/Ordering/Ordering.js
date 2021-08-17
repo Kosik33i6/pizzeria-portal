1f10b3759c6d0f4de973536e2e845b7f95695306
Kos Krzysztof <krzysztof.kos33@gmail.com>
2021-08-17 16:59:25 +0200

    Add new links for order and tables

--- src/components/views/Ordering/Ordering.js
+++ src/components/views/Ordering/Ordering.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import Order from '../Order/Order';
+import {Link} from 'react-router-dom';
 import styles from './Ordering.scss';
 
 const Ordering = () => {
     return (
         <div className={styles.component}>
             <h2>Ordering</h2>
+            <Link to={`${process.env.PUBLIC_URL}/ordering/order/:21`}>Order</Link>
+            <Link to={`${process.env.PUBLIC_URL}/ordering/new`}>New order</Link>
         </div>
     );
 }
--- src/components/views/Tables/Tables.js
+++ src/components/views/Tables/Tables.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 import styles from './Tables.scss';
 
 const Tables = () => {
     return (
         <div className={styles.component}>
             <h2>tables</h2>
+            <Link to={`${process.env.PUBLIC_URL}/tables/booking/:1`}>Booking</Link>
+            <Link to={`${process.env.PUBLIC_URL}/tables/event/:2`}>Event</Link>
         </div>
     );
 }
